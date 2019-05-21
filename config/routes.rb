2006e7e5b0cc7df74109379b2c1a94e7a8ea4667
Douglas Berkley <douglasmberkley@gmail.com>
2019-05-21 11:35:41 +0900

    morning lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # VERB "PATH", to: "CONTROLLER#ACTION", AS: :PREFIX
+  # CRUD
+  # read all
+  get 'restaurants', to: 'restaurants#index', as: :restaurants
+  # new
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # read one
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # create
+  post 'restaurants', to: 'restaurants#create'
+
+  # edit
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # update
+  patch 'restaurants/:id', to: 'restaurants#update'
+
+  # delete
+  delete 'restaurants/:hey_guys_delete', to: 'restaurants#destroy'
+
 end
+
+
+
+
+
+
+
+
+
